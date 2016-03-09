16e6d841eeb1fc51eec672b28ceef0e1bd3d3d58
ongzhimin <zhimin_93@hotmail.sg>
2016-03-09 22:25:40 +0800

    Added files via upload

---
+++ public/js/script.js
@@ -0,0 +1,27 @@
+var linkedApp = angular.module('linkedApp', ['ngRoute']);
+
+linkedApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : '../list.html',
+            controller  : 'mainController'
+        })
+
+        // route for the product page
+        .when('/product', {
+            templateUrl : '../product.html',
+            controller  : 'productController'
+        })
+
+});
+
+// create the controller and inject Angular's $scope
+linkedApp.controller('mainController', function($scope) {
+
+});
+
+linkedApp.controller('productController', function($scope) {
+
+});
