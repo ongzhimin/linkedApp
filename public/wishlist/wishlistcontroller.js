bea7d30f2b3ff529383a6a4bdcbf96052de529ec
carinahuqunqun <qunqun.hu.2012@sis.smu.edu.sg>
2016-03-25 00:34:15 +0800

    updated wishlist display date format

--- public/wishlist/wishlistcontroller.js
+++ public/wishlist/wishlistcontroller.js
@@ -4,10 +4,13 @@ linkedApp.controller('wishlistController', function ($scope, $firebase) {
     var sync = $firebase(ref);
     $scope.items = sync.$asArray();
     var favouritesArray=[]
+    var endDate
+    var endTime
 
     $scope.items.$loaded(function(data) {
         for(var i=0;i<$scope.items.length;i++){
-            favouritesArray.push({ShopName:$scope.items[i].ShopName,PromotionTitle1:$scope.items[i].PromotionTitle1,TCText1:$scope.items[i].TCText1,End:new Date($scope.items[i].End*1000),Image:$scope.items[i].Image})
+            favouritesArray.push({ShopName:$scope.items[i].ShopName,PromotionTitle1:$scope.items[i].PromotionTitle1,TCText1:$scope.items[i].TCText1,EndDate:new Date($scope.items[i].End*1000).toString().split('G')[0],Image:$scope.items[i].Image})
+
         }
         $scope.favourites=favouritesArray
     });
